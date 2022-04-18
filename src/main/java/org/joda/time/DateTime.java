1e2145ff43f1d9141607b0166413a67c29770701
Pavel Sher <pavel.sher@gmail.com>
2022-04-18 17:20:39 +0200

    breaking compilation for test purposes

--- src/main/java/org/joda/time/DateTime.java
+++ src/main/java/org/joda/time/DateTime.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.joda.time;
+p ackage org.joda.time;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
